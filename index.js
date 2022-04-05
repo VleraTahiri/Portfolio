b7f43ff3f49eecacc9570949c26b0045a358e5f3
Vlera-Tahiri <tahirivlere@gmail.com>
2022-04-05 18:29:17 +0200

    +

---
+++ index.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        if(this.scrollY > 500){
+            $('.butoni-scrollup').addClass("show");
+        }else{
+            $('.butoni-scrollup').removeClass("show");
+        }
+    });
+
+   
+    $('.butoni-scrollup').click(function(){
+        $('html').animate({scrollTop: 0});
+        
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    
+    $('.menu-butoni').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-butoni i').toggleClass("active");
+    });
+
+   
+    var typed = new Typed(".typing", {
+        strings: [ "Developer",  "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Developer",  "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
